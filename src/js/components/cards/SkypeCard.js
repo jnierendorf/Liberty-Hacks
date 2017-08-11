ee8d069fc0c9953c11eb9b84ca9e0df55c62b966
jtheo314 <jtheohar@gmail.com>
2017-08-11 14:22:34 -0400

    added skype components and cards

--- src/js/components/cards/SkypeCard.js
+++ src/js/components/cards/SkypeCard.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import icon from '../../../media/skype.png';
+
 export default class SkypeCard extends React.Component {
   render() {
     return (
+<a href="../skype/skype1.js">
       <div className="Card">
-        <p>This is the skype card!</p>
-      </div>
+
+        <div className="CardTitle">
+          Skype/Lync Help
+        </div>
+        <div className="CardContainer">
+          <div className="CardIcon">
+            <img src={icon} height="100%" width="100%"></img>
+          </div>
+          <div className="CardContent">
+            <ul>
+              <li>Audio</li>
+              <li>Video</li>
+              <li>Instant-message</li>
+            </ul>
+          </div>
+          </div>
+        </div>
+        </a>
     );
   }
 }
