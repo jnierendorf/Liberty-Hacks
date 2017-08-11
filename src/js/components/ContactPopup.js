9096897ad8244a97764afd62c4771885e3becf89
jnierendorf <jnierendorf@umass.edu>
2017-08-11 13:37:00 -0400

    more work on contact popup screen

--- src/js/components/ContactPopup.js
+++ src/js/components/ContactPopup.js
@@ -3,10 +3,43 @@ import React from 'react';
 export default class ContactPopup extends React.Component {
   render() {
     return (
-      <div className='popup'>
-        <div className='popup_inner'>
+      <div className='Popup'>
+        <div className='PopupInner'>
+          <div className='PopupExit' onClick={this.props.closePopup}>X</div>
           <h1>Contact the IT Help Desk</h1>
-        <button onClick={this.props.closePopup}>close me</button>
+          <h3>Estimated wait time:</h3>
+          <h2>6 Minutes</h2>
+          <form>
+            <div className="ContactInput">
+              <span>N-Number:</span>
+              <input type="text" name="n-number"/>
+            </div>
+            <div className="ContactInput">
+              <span>Callback #:</span>
+              <input type="text" name="callback"/>
+            </div>
+            <div className="ContactInput ContactSummary">
+              <span>Summary of Issue:</span>
+              <textarea type="text" name="summary"/>
+            </div>
+            <div className="ContactInput">
+              <span>Urgency:</span>
+              <div className="ContactUrgencyInput">
+                <input type="radio" name="urgency" value="high"/>
+                <span>High</span>
+              </div>
+              <div className="ContactUrgencyInput">
+                <input type="radio" name="urgency" value="med"/>
+                <span>Med</span>
+              </div>
+              <div className="ContactUrgencyInput">
+                <input type="radio" name="urgency" value="low" checked="checked"/>
+                <span>Low</span>
+              </div>
+            </div>
+            <br/><br/>
+            <input type="submit" value="Submit Request" className="ContactButton"/>
+          </form>
         </div>
       </div>
     );
