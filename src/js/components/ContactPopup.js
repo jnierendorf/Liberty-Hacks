8c3c96f2a4c3a51b966ae2556acf42cb44436875
jnierendorf <jnierendorf@umass.edu>
2017-08-14 10:22:56 -0400

    finalized initial contact button creation

--- src/js/components/ContactPopup.js
+++ src/js/components/ContactPopup.js
@@ -10,18 +10,22 @@ export default class ContactPopup extends React.Component {
           <h3>Estimated wait time:</h3>
           <h2>6 Minutes</h2>
           <form>
+
             <div className="ContactInput">
               <span>N-Number:</span>
               <input type="text" name="n-number"/>
             </div>
+
             <div className="ContactInput">
               <span>Callback #:</span>
               <input type="text" name="callback"/>
             </div>
+
             <div className="ContactInput ContactSummary">
               <span>Summary of Issue:</span>
               <textarea type="text" name="summary"/>
             </div>
+
             <div className="ContactInput">
               <span>Urgency:</span>
               <div className="ContactUrgencyInput">
@@ -33,12 +37,31 @@ export default class ContactPopup extends React.Component {
                 <span>Med</span>
               </div>
               <div className="ContactUrgencyInput">
-                <input type="radio" name="urgency" value="low" checked="checked"/>
+                <input type="radio" name="urgency" value="low" defaultChecked/>
                 <span>Low</span>
               </div>
             </div>
-            <br/><br/>
+
+            <div className="ContactInput">
+              <span>Callback Time:</span>
+              <div>
+                <div className="ContactTimeInput">
+                  <input type="radio" name="callback" value="asap" defaultChecked/>
+                  <span>ASAP</span>
+                </div>
+                <div className="ContactTimeInput">
+                  <input type="radio" name="callback" value="custom"/>
+                  <span>Custom</span>
+                </div>
+              </div>
+              <div className="ContactTimeDisplay">
+                2:30 PM
+              </div>
+            </div>
+
+            <br/><br/><br/>
             <input type="submit" value="Submit Request" className="ContactButton"/>
+
           </form>
         </div>
       </div>
