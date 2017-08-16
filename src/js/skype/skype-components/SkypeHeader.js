44730baa9fec703a763393211d1fae0f9f4bd2cc
jtheo314 <jtheohar@gmail.com>
2017-08-16 14:56:13 -0400

    back button to all non-home pages

--- src/js/skype/skype-components/SkypeHeader.js
+++ src/js/skype/skype-components/SkypeHeader.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import icon from '../../../media/backbutton.png';
+import '../../../css/App.css';
+import { Link } from 'react-router-dom';
+
 export default class SkypeHeader extends React.Component {
   render() {
     return (
-      <div className="Header">
-        <h1>How can we help you with Skype?</h1>
+      <div>
+        <div className="Header">
+          <h1>How can we help you with Skype?</h1>
+        </div>
+        <div className="BackButton">
+          <Link to='../'><img src={icon} height="55px"></img></Link>
+        </div>
       </div>
     );
   }
