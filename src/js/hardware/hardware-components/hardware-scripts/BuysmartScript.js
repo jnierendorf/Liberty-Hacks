471d51ddfa24765b65896eaef2ad1f2c84802bc0
jtheo314 <jtheohar@gmail.com>
2017-08-25 13:44:38 -0400

    hardware and buysmart fix

--- src/js/hardware/hardware-components/hardware-scripts/BuysmartScript.js
+++ src/js/hardware/hardware-components/hardware-scripts/BuysmartScript.js
@@ -8,17 +8,9 @@ export default class BuysmartScript extends React.Component {
       <div className="Script">
       <ol>
 <li>Login to buySmart.</li>
-<li>On the left-hand side of the main buySmart page that displays, beneath the Common Action subject heading, click on Requisition.
-buySmart Requisition</li>
-<li>Under Shop by Category on the left hand side of your screen, navigate to Computers and IT equipment > Hardware and Peripherals.
-buySmart Compters and Peripherals
-NOTE: If you are a Dynamic Desktop (VDI) particpant, review the "Thin Client" options and follow these instructions.</li>
-<li>Prior to making your purchase decision, review the following table for Windows operating system compatibility.  Ensure that the device your purchase will support your required Windows operating system.
-Device Type	Windows 7 32 Bit	Windows 10 64 Bit
-HP 840 G4 Laptop 1	Not Supported	Fully Supported 2
-HP 1040 G3 Laptop	Fully Supported	Fully Supported
-HP Elite 1012 X2 Tablet	Not Supported	Fully Supported
-HP Elite 800 G3 Desktop	Partial Support 3	Partial Support 3</li>
+<li>On the left-hand side of the main buySmart page that displays, beneath the Common Action subject heading, click on Requisition.</li>
+<li>Under Shop by Category on the left hand side of your screen, navigate to Computers and IT equipment > Hardware and Peripherals.</li>
+<li>Prior to making your purchase decision, review the following table for Windows operating system compatibility.  Ensure that the device your purchase will support your required Windows operating system.</li>
 <li>Select the item you want to order, then click the product name to see a detailed description of the item.  If satisfied, click Add to Cart.
 TIP: To add additional items to you order, select ‘Continue Shopping’ at the top of the page and continue to add items into the cart.</li>
 <li>Once you have added your computer to the cart, you can add additional items such as keyboard, mouse, docking station, etc. to the cart by choosing from the categories on the left hand menu.  Continue to add items to the cart, as needed.</li>
@@ -26,20 +18,17 @@ TIP: To add additional items to you order, select ‘Continue Shopping’ at the
 <li>If satisfied, click Proceed to Checkout.
 TIP: All fields that are preceded with an asterisk (*) are required fields. If you leave these fields blank, you will neither be able to proceed to the next page nor complete your requisition.</li>
 <li>Upon checking out, enter a title for your order.</li>
-<li>Are you ordering a computer for yourself, or on behalf of someone else?
-Yourself: Continue.
-On behalf of someone else: Skip to step 12.
+<li>If you are ordering a computer for yourself, continue to the next step. If for someone else, skip to step 12.
 </li>
 <li>If it is not already present, choose your name from the dropdown menu to the right of the On Behalf Of field. Then, skip to step 16.</li>
 <li>Choose Search more from the dropdown menu to the right of the On Behalf Of field.</li>
 <li>When you are automatically redirected to the Choose Value for On Behalf Of page, type the last name of the person on whose behalf you are ordering the computer into the Name field.</li>
 <li>Click on the Search button. A list of employees with that last name will display.</li>
 <li>Click the Select button next to the name of the person on whose behalf you are ordering this computer.</li>
-<li>Review your order for accuracy.
-TIP: The cost center the device is being billed to can be updated by going to Line Items, then choosing ‘Edit’ from the Actions button. From there, navigate to ‘Accounting – by Line Item’ and make any needed updates.</li>
-<li>Select Submit to complete your order.
+<li>Review your order for accuracy.</li>
+<li>Select Submit to complete your order.</li>
 CAUTION: Laptops in the "Specialty Laptop" and "Tablet" categories are intended for special use cases only, and not for general orders.  Devices in both of these categories require special approval above and beyond standard approval for the related expense.
-NOTE: A new level of approval is required for laptop orders called DCC Commodity Approver, which is required before the approval request is sent to the approving manager.  As a result, it may take slightly longer to process orders.</li>
+NOTE: A new level of approval is required for laptop orders called DCC Commodity Approver, which is required before the approval request is sent to the approving manager.  As a result, it may take slightly longer to process orders
 </ol>
       </div>
     );
